2031ac1be6b0210fb61e92507a8a863330c0850c
Nida Rizwan <143074620+NidzR@users.noreply.github.com>
2024-08-27 00:21:08 +0500

    Django-Python-Todo-App-Project
    "This Todo App project, built using Django and Python, provides an easy and user-friendly platform for users to efficiently manage and organize their daily tasks. The app offers CRUD (Create, Read, Update, Delete) functionality, giving complete control over each task throughout its lifecycle. With user authentication and session management, the app ensures a secure and personalized task management experience. This project is ideal for learning and practical implementation, helping developers understand both the basics and advanced features of the Django framework

---
+++ models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class TODOO(models.Model):
+    srno=models.AutoField(primary_key=True,auto_created=True)
+    title = models.CharField(max_length=25)
+    date=models.DateTimeField(auto_now_add=True)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
